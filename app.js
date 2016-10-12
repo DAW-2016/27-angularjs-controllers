86cb1a8578c81b7db0f6794badaeefb085d0b3d1
LucasWander <lucaswf48@gmail.com>
2016-10-12 17:13:16 -0300

    Exercicios 2,3 e 4

--- app.js
+++ app.js
@@ -25,3 +25,103 @@ app.controller('TemperaturaController', function() {
   }
 
 });
+
+app.controller('impostoController', ['$scope', function($scope) {
+
+    $scope.salario = 2000;
+
+
+    $scope.calculaImposto = function() {
+
+        if($scope.salario < 2000){
+            return $scope.salario * 0.1;
+        }
+        else if($scope.salario >= 2000 && $scope.salario <= 3500){
+            return $scope.salario * 0.2;
+        }
+
+        return $scope.salario * 0.3;
+
+    };
+
+}]);
+
+app.controller('reviewController', ['$scope', function($scope) {
+
+    $scope.numeroEstrelas = 1;
+    $scope.comentarioAtual = "";
+    $scope.comentarios = [];
+
+    $scope.envia = function(){
+
+        $scope.comentarios.push($scope.comentarioAtual);
+        $scope.comentarioAtual = "";
+        console.log($scope.comentarios);
+    }
+}]);
+
+app.controller('contactController', ['$scope', function($scope) {
+
+    $scope.contatos = [];
+
+    $scope.contato = {};
+    $scope.contato.nome = "";
+    $scope.contato.telefone = "";
+    $scope.contato.email = "";
+    $scope.contato.idn = -1;
+
+    $scope.numeroContatos = 0;
+
+    $scope.deleta = function (deleteId) {
+
+        var index = $scope.contatos.indexOf(deleteId);
+
+        $scope.contatos[index] = null;
+    };
+
+
+    $scope.carregaFormulario = function (alteraId) {
+
+        var index = $scope.contatos.indexOf(alteraId);
+        var alterando = $scope.contatos[index];
+
+        $scope.contato.nome = alterando.nome;
+        $scope.contato.telefone = alterando.telefone;
+        $scope.contato.email = alterando.email;
+        $scope.contato.idn = alterando.idn;
+    }
+
+    $scope.altera = function (){
+
+        console.log($scope.contato.idn);
+
+        if(($scope.contato.idn != -1)){
+
+            var copy = Object.assign({}, $scope.contato);
+
+            $scope.contatos[$scope.contato.idn].nome = copy.nome;
+            $scope.contatos[$scope.contato.idn].telefone = copy.telefone;
+            $scope.contatos[$scope.contato.idn].email = copy.email;
+
+            $scope.contato.nome = "";
+            $scope.contato.telefone = "";
+            $scope.contato.email = "";
+            $scope.contato.idn = -1;
+        }
+    }
+
+    $scope.envia = function(){
+
+        $scope.contato.idn = $scope.numeroContatos;
+        var copy = Object.assign({}, $scope.contato);
+
+        $scope.numeroContatos++;
+        $scope.contato.nome = "";
+        $scope.contato.telefone = "";
+        $scope.contato.email = "";
+        $scope.contato.idn = -1;
+        $scope.contatos.push(copy);
+
+        //console.log($scope.contato);
+    }
+}]);
